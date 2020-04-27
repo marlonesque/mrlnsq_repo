61833bdc77fe01ae9586a3d29b4b4453fa023a8b
Marlon Ausby <lacboi2k7@gmail.com>
2020-04-27 12:23:04 -0400

    ex5

---
+++ Python The Hard Way/ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Marlon L. Ausby'
+my_age = 40 # not a lie
+my_height = 70 # inches
+my_weight = 225 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"Actually that's a bit heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on what was eaten.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
